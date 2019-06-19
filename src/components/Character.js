d5caae315f020d408c2cac8abb79a7d98b6a43a8
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-19 16:45:26 +0100

    Styled the character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import { object } from 'prop-types';
+import styled from 'styled-components';
 
-const Character = ({ character }) => <li>{character.name}</li>;
+const StyledCharacter = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 500px;
+  border-radius: 2px;
+  background: rgb(24, 23, 17);
+  color: rgb(240, 227, 158);
+  margin: 20px 0;
+  padding: 20px;
+
+  img {
+    width: 50px;
+    height: 50px;
+  }
+
+  p {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 25px;
+    margin-left: 30px;
+  }
+`;
+
+const Character = ({ character }) => (
+  <StyledCharacter>
+    <img
+      src="https://image.flaticon.com/icons/png/512/922/922854.png"
+      alt="light_saber"
+    />
+    <p>{character.name}</p>
+  </StyledCharacter>
+);
 
 Character.propTypes = {
   character: object.isRequired,
